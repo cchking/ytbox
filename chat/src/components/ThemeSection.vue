730ee6264728b68cc04fbd3c731237592d66de64
mtx <138717695+cchking@users.noreply.github.com>
2025-01-22 22:51:55 +0800

    Update ThemeSection.vue

--- chat/src/components/ThemeSection.vue
+++ chat/src/components/ThemeSection.vue
@@ -1,15 +1,23 @@
-<!-- src/components/ThemeSection.vue -->
 <template>
   <div class="h-[10%] p-4">
-    <h2 class="text-xl font-bold mb-2">样式</h2>
-    <div class="flex space-x-2">
-      <button
-        v-for="color in colors"
-        :key="color"
-        class="w-8 h-8 rounded-full"
-        :class="`bg-${color}-500`"
-        @click="$emit('change', color)"
-      ></button>
+    <h2 class="text-xl font-bold mb-2">开源</h2>
+    <div class="flex flex-col space-y-2">
+      <a
+        href="https://github.com/cchking/ytbox"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src="@/assets/images/UilGithub.svg" alt="GitHub" class="w-6 h-6" />
+      </a>
+      <div class="flex space-x-2">
+        <button
+          v-for="color in colors"
+          :key="color"
+          class="w-8 h-8 rounded-full"
+          :class="`bg-${color}-500`"
+          @click="$emit('change', color)"
+        ></button>
+      </div>
     </div>
   </div>
 </template>
